3b6cf89dc89330e681eced25fd73025eb6858bc3
heather Reeve <heather.reeve@scope.org.uk>
2024-07-29 11:25:57 +0100

    added movies.ejs

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import Movie from '../models/Movie.js'
 
 const app = express()
 
+app.use(express.static('public'))
+app.set('views', 'views') 
+app.set ('view engine', 'ejs')
 // TODO: add your endpoints here
+app.get('/', async (req,res) =>{
+const movies = await Movie.findAll()
+res.render('index', {movies})
+})
+
+app.get('/movie/:movieID', async (req, res) =>{
+    const movieID = req.params.movieID
+    
+})
 
 export default app
